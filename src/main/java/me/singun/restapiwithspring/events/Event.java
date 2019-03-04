e7d77c69cfa8038bbb36442f646cdea5426744fb
singun <daewook.shin@gmail.com>
2019-03-04 21:31:50 +0900

    POST /api/events returns 201

--- src/main/java/me/singun/restapiwithspring/events/Event.java
+++ src/main/java/me/singun/restapiwithspring/events/Event.java
@@ -2,6 +2,7 @@ package me.singun.restapiwithspring.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder
@@ -10,7 +11,9 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
+	@Id @GeneratedValue
 	private Integer id;
 	private String name;
 	private String description;
@@ -24,5 +27,6 @@ public class Event {
 	private int limitOfEnrollment;
 	private boolean offline;
 	private boolean free;
+	@Enumerated(EnumType.STRING) // ordinal 은 enum 의 순서대로 숫자가 생성되는데, enum 의 순서가 변경되면 데이터가 꼬일 수 있음
 	private EventStatus eventStatus;
 }
--- src/test/java/me/singun/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/singun/restapiwithspring/events/EventControllerTests.java
@@ -1,15 +1,23 @@
 package me.singun.restapiwithspring.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -27,11 +35,34 @@ public class EventControllerTests {
 	// 요청을 만들수 있고, 응답을 테스트 해볼 수 있음
 	// 웹서버를 띄우지 않기 때문에 빠름
 
+	@MockBean
+	EventRepository eventRepository;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
 	@Test
 	public void createEvent() throws Exception {
-		mockMvc.perform(post("/api/events/")
-			.contentType(MediaType.APPLICATION_JSON_UTF8)
-			.accept(MediaTypes.HAL_JSON)) // 이런 응답을 받고 싶다!
-			.andExpect(status().isCreated());
+		Event event = Event.builder()
+			.name("Spring")
+			.description("REST API Development with Spring")
+//			.beginEnrollmentDateTime(LocalDateTime.of(2019, 03, ))
+			.basePrice(100)
+			.maxPrice(200)
+			.limitOfEnrollment(100)
+			.location("D2 Startup Factory")
+			.build();
+		event.setId(10);
+		Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+		mockMvc.perform(
+			post("/api/events/")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.accept(MediaTypes.HAL_JSON)
+				.content(objectMapper.writeValueAsString(event)))
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("id")
+				.exists());
 	}
 }
