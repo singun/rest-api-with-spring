6acc091ce9fd08a16ad36fc549907277c7f31597
singun <daewook.shin@gmail.com>
2019-03-04 20:45:18 +0900

    first commit

---
+++ src/main/java/me/singun/restapiwithspring/events/EventStatus.java
@@ -0,0 +1,5 @@
+package me.singun.restapiwithspring.events;
+
+public enum EventStatus {
+	DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED;
+}
---
+++ src/test/java/me/singun/restapiwithspring/RestApiWithSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package me.singun.restapiwithspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApiWithSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
